331ea5304a2e1b96637e1b625ca6d6e4008311cc
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-25 00:39:44 +0100

    init Application

---
+++ core/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Application
+ * 
+ * @author Hirekaan Micheal Hemen <hirekaan.micheal@gmail.com>
+ * @package app\core
+ */
+class Application
+{
+
+    public Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
+}
