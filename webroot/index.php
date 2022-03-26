8a505175bc1b8282dbd9a8d1fad36f258eef7c28
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-26 01:34:17 +0100

    cwd added to Application constructor

--- webroot/index.php
+++ webroot/index.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 
 use app\core\Application;
 
-$application = new Application();
+$application = new Application(dirname(__DIR__));
 
 $application->router->get("/", "home");
 
