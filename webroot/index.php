c5cfeca148c5697a95ac6e8ab9205805a4a84e37
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-26 01:05:37 +0100

    added new routes

--- webroot/index.php
+++ webroot/index.php
@@ -1,17 +1,15 @@
 <?php
 
-require_once (__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/../vendor/autoload.php");
 
 use app\core\Application;
 
 $application = new Application();
 
-$application->router->get("/", function () {
-    return "Hello World";
-});
-$application->router->get("/contact", function () {
-    return "contact page";
-});
+$application->router->get("/", "home");
+
+$application->router->get("/contact", "contact");
+
 $application->router->get("/drainage/plumber", function () {
     return "drainage page";
 });
