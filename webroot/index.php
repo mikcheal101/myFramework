5bc4513f55f376380fc6a877c9b6df28b856fc3a
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 00:06:56 +0100

    added post route for contact

--- webroot/index.php
+++ webroot/index.php
@@ -7,11 +7,9 @@ use app\core\Application;
 $application = new Application(dirname(__DIR__));
 
 $application->router->get("/", "home");
-
 $application->router->get("/contact", "contact");
-
-$application->router->get("/drainage/plumber", function () {
-    return "drainage page";
+$application->router->post("/contact", function() {
+    return "handling post request";
 });
 
 // $application->useRouter($router);
