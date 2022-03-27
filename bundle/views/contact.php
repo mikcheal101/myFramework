b84727f1d552efcf817e3aa751e408752c94b2d4
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 00:00:53 +0100

    sample contact page created

--- bundle/views/contact.php
+++ bundle/views/contact.php
@@ -1,1 +1,22 @@
 <h1>This is the contact page</h1>
+<div class="row">
+    <div class="col">
+
+        <form class="form">
+            <div class="form-group">
+                <label for="emailSubject">Subject:</label>
+                <input name="emailSubject" class="form-control" type="text" />
+            </div>
+            <div class="form-group">
+                <label for="emailAddress">Email Address:</label>
+                <input name="emailAddress" class="form-control" type="email" />
+            </div>
+            <div class="form-group">
+                <label for="emailBody">Body:</label>
+                <textarea name="emailBody" class="form-control"></textarea>
+            </div>
+            <hr class="dropdown-divider" />
+            <button type="submit" class="btn btn-primary">submit</button>
+        </form>
+    </div>
+</div>
