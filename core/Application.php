b95a0e49564aad5c417e12d2dfdcb08cc6043cdb
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 00:02:02 +0100

    updated Application

--- core/Application.php
+++ core/Application.php
@@ -15,13 +15,21 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public static Application $app;
 
+    /**
+     * Application constructor
+     * 
+     * @param string $routePath
+     */
     public function __construct(string $rootPath)
     {
         self::$ROOT_DIRECTORY = $rootPath;
+        self::$app = $this;
+
         $this->request = new Request();
         $this->response = new Response();
-        $this->router = new Router($this->request);
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run()
