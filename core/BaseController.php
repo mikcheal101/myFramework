4cb829c40d075d0d6936db7a8718aa06a724c752
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 00:51:39 +0100

    BaseController defined

---
+++ core/BaseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\core;
+
+/**
+ * class BaseController
+ * 
+ * @author Hirekaan Micheal Hemen <hirekaan.micheal@gmail.com>
+ * @package app\core
+ */
+
+class BaseController
+{
+
+    /**
+     * Render a page
+     * 
+     * @param string $page
+     * @param array $parameters - Parameters to pass to the page
+     * 
+     * @return string|false  
+     */
+    protected function render(string $page, array $parameters = []): string | false
+    {
+        return Application::$app->router->renderView($page, $parameters);
+    }
+}
