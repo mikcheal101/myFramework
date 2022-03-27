d0763f2712cc91cd2e9f1ff177b9e9bfebe9205a
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 00:01:09 +0100

    404 page created

---
+++ bundle/views/responsePages/_404Page.php
@@ -0,0 +1,5 @@
+<div class="row mt-3">
+    <div class="offset-3 col-6 text-center p-5">
+        <h5 class="bg-light border p-5 rounded text-secondary">Opps! Page Not Found!</h5>
+    </div>
+</div>
