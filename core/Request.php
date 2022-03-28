34a343a518a260ec3e1765e8269bfa306df1acd5
hirekaan.hemen <hirekaan.hemen@konga.com>
2022-03-28 01:11:18 +0100

    request getbody defined

--- core/Request.php
+++ core/Request.php
@@ -11,25 +11,52 @@ namespace app\core;
 
 class Request
 {
+    /**
+     * Method to get the path visited.
+     * 
+     * @return string $path
+     */
     public function getPath(): string
     {
         $path = $_SERVER["REQUEST_URI"] ?? "/";
         $position = strpos($path, "?");
 
-        if ($position === false)
-        {
+        if ($position === false) {
             return $path;
         }
 
         return substr($path, 0, $position);
     }
 
+    /**
+     * Method to get the verb used in accessing the page.
+     * 
+     * @return string method
+     */
     public function getMethod(): string
     {
-        return strtolower( $_SERVER["REQUEST_METHOD"] );
+        return strtolower($_SERVER["REQUEST_METHOD"]);
     }
 
-    public function getBody()
+    /**
+     * Method to get the request data
+     */
+    public function getBody(): array
     {
+        $body = [];
+
+        if ($this->getMethod() === "post") {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        if ($this->getMethod() === "post") {
+            foreach ($_POST as $key => $value) {
+                $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+
+        return $body;
     }
 }
